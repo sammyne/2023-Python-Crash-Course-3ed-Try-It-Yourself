c39a1d643e4ef4d70599288a12addc9bba26710d
sammyne <xiangminli-li@qq.com>
2025-04-26 11:03:48 +0800

    feat(chapter04): solve all

---
+++ chapter04/04.py
@@ -0,0 +1,6 @@
+# 4-4. One Million: Make a list of the numbers from one to one million, and then
+# use a for loop to print the numbers. (If the output is taking too long, stop it by
+# pressing CTRL-C or by closing the output window.)
+
+for v in range(1, 1_000_001):
+    print(v)
