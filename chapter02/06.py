61c0c18034a8f29a30786b6fe8373f5ce0e2fcb8
sammyne <xiangminli-li@qq.com>
2025-04-26 12:03:45 +0800

    feat(chapter02): solve all

---
+++ chapter02/06.py
@@ -0,0 +1,6 @@
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous
+# person’s name using a variable called famous_person. Then compose your mes-
+# sage and represent it with a new variable called message. Print your message.
+
+famous_person = "Albert Einstein"
+print(f"{famous_person} once said, \"A person who never made a mistake never tried anything new.\"")
---
+++ chapter02/09.py
@@ -0,0 +1,11 @@
+# 2-9. Number Eight: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8. Be sure to enclose your operations
+# in print() calls to see the results. You should create four lines that look like this:
+# print(5+3)
+# Your output should be four lines, with the number 8 appearing once on
+# each line.
+
+print(5+3)
+print(15-7)
+print(2*4)
+print(24/3)
